b526f066b547bdc632b67c2babde0de0b18dcf85
Animesh Sitoula <88762554+animeshsitoula@users.noreply.github.com>
2026-07-26 08:51:45 +0545

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://trivia-ehw5.onrender.com";
 
 async function loadActiveWeek() {
     const label = document.getElementById("week-label");
