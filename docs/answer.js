c5573b9aa4e9e7cd82fc7ae3c979d4f747bb4264
Animesh Sitoula <88762554+animeshsitoula@users.noreply.github.com>
2026-07-26 08:52:04 +0545

    Update answer.js

--- docs/answer.js
+++ docs/answer.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://trivia-ehw5.onrender.com";
 
 const params = new URLSearchParams(window.location.search);
 const subject = params.get("subject");
